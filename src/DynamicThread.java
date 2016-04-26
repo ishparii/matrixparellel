34d36608df0b907477aa8d31d4ba7c9c25c34590
annezhao <K02200059>
2016-04-26 15:10:25 -0500

    Merge branch 'master' of https://bitbucket.org/annezhao1010/matrixparellel
    # Conflicts:
    #	src/StaticScheduling.java

--- src/DynamicThread.java
+++ src/DynamicThread.java
@@ -1,3 +1,7 @@
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
 /**
  * Created by Ilona on 4/25/2016.
  */
@@ -5,8 +9,9 @@ public class DynamicThread implements Runnable {
     int[][] A;
     int[][] B;
     int[][] C;
-    static int row;
+    static int row = 0;
     int size;
+    private static Object lock = new Object();
 
     public DynamicThread(int[][] A, int[][] B, int[][] C, int size) {
         this.A = A;
@@ -17,8 +22,14 @@ public class DynamicThread implements Runnable {
 
     @Override
     public void run() {
-        int i = 0;
+        int i;
         while (row < size) {
+
+            synchronized (lock) {
+                i = row;
+                row ++;
+            }
+
             for (int j=0; j<size; j++) {
                 for (int k=0; k<size; k++) {
                     C[i][j] = C[i][j] + (A[i][k]*B[k][j]);
